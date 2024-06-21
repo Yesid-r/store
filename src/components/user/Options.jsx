72519ec9e394ba9438e4fb4dd2b843ab825bd2e8
Yesid Rincon <dairo.rincon@uptc.edu.co>
2024-06-21 14:37:04 -0500

    bug fix logout

--- src/components/user/Options.jsx
+++ src/components/user/Options.jsx
@@ -83,7 +83,7 @@ const Options = () => {
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
                             </svg>
                         </span>
-                        <span onClick={() => logout}>Cerrar sesion</span>
+                        <span onClick={logout}>Cerrar sesion</span>
                     </li>
                 </ul>
             </div>
